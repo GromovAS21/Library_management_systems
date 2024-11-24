d232e27201d4f155203f308687b4bb8175c75364
Anton Gromov <seed14112@yandex.ru>
2024-11-24 20:23:37 +0300

    Добавление функции смены статуса

--- src.py
+++ src.py
@@ -4,7 +4,6 @@ from class_books import Book
 from file_saver import write_file, read_file
 from services import out_red_text, out_green_text
 
-
 def adding_book():
     """
     Добавление книги
@@ -68,12 +67,30 @@ def views_books():
     for num, value in enumerate(books, 1):
         print(num, Book(**value))
 
+def change_status():
+    """
+    Замена статуса (в наличии, выдана)
+    """
 
-
-
-
-
+    books = read_file()
+    book_id = input("Введите id книги для изменения статуса: ")
+    if book_id.isdigit():
+        indicator = 1
+        for book in books:
+            if int(book_id) == book["id"]:
+                if book["status"] == "в наличии":
+                    book["status"] = "выдан"
+                else:
+                    book["status"] = "в наличии"
+                print("Статус книги с id {} изменен!".format(book["id"]))
+                with open("db_book.json", "w", encoding="utf-8") as file:
+                    json.dump(books, file, ensure_ascii=False, indent=5)
+                indicator = 0
+        if indicator:
+            print("Книга не найдена!")
+    else:
+        print(out_red_text("Введен некорректный id!"))
 
 
 if __name__ == "__main__":
-    views_books()
+    change_status()
