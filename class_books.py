193506dc15946b79b06a97fbaafccd238c296368
Anton Gromov <seed14112@yandex.ru>
2024-11-24 23:04:59 +0300

    Добавление set_book_id

--- class_books.py
+++ class_books.py
@@ -2,15 +2,19 @@ class Book:
     """
     Класс книги
     """
-    book_id = 1
+    BOOK_ID = 0
 
     def __init__(self, title:str, author:str, year:int, id=None, status="в наличии"):
-        self.id = Book.book_id if id is None else id
+        self.id = self.set_book_id() if id is None else id
         self.title = title
         self.author = author
         self.year = year
         self.status = status
-        Book.book_id += 1
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.id}, {self.title}, {self.author}, {self.year}, {self.status})"
+
+    @classmethod
+    def set_book_id(cls):
+        cls.BOOK_ID += 1
+        return cls.BOOK_ID
