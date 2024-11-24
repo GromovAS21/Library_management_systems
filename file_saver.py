81486c7ccd4f8de72e946d5642830a074fd3448f
Anton Gromov <seed14112@yandex.ru>
2024-11-24 21:24:55 +0300

    изменение функции записи

--- file_saver.py
+++ file_saver.py
@@ -13,19 +13,11 @@ def read_file() -> list:
         except json.decoder.JSONDecodeError:
             return []
 
-def write_file(book:Book) -> None:
+def write_file(books:list) -> None:
     """
     Запись в json файл
     """
-    books = read_file()
-    book = {
-        "id": book.id,
-        "title": book.title,
-        "author": book.author,
-        "year": book.year,
-        "status": book.status
-    }
-    books.append(book)
+
     with open("db_book.json", "w", encoding="utf-8") as file:
         json.dump(books, file, ensure_ascii=False, indent=5)
 
