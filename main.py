05f971bbfc373850a57e00216a09935146f36d80
Anton Gromov <seed14112@yandex.ru>
2024-11-24 21:25:52 +0300

    Добавление точки входа для запуска приложения

---
+++ main.py
@@ -0,0 +1,22 @@
+from services import out_red_text
+from src import views_books, adding_book, delete_book, search_book, change_status
+
+
+def main():
+    while True:
+        input_user = input("\nВведите команду: ").lower().strip()
+        if input_user == "list":
+            print(views_books())
+        elif input_user == "add":
+            print(adding_book())
+        elif input_user == "delete":
+            print(delete_book())
+        elif input_user == "search":
+            print(search_book())
+        elif input_user == "change":
+            print(change_status())
+        else:
+            print(out_red_text("Неизвестная команда!"))
+
+if __name__ == "__main__":
+    main()
