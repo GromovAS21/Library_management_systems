fa44503202714a30542cc1b5cd5dd335b546457e
Anton Gromov <seed14112@yandex.ru>
2024-11-24 14:31:46 +0300

    добавление класса book

---
+++ class_books.py
@@ -0,0 +1,20 @@
+class Book:
+    """
+    Класс книги
+    """
+    book_id = 1
+    def __init__(self, title:str, author:str, year:int, status="в_наличии"):
+        self.id = self.book_id
+        self.title = title
+        self.author = author
+        self.year = year
+        self.status = status
+        Book.book_id += 1
+
+    def __str__(self):
+        """
+        Строчное представление класса
+        """
+
+        return "{}. {} - {}, {} г. {}".format(self.id, self.title, self.author, self.year, self.status)
+
